50a9302c3b787e38822b6271b0c734963a65e873
sakhsi chourasiya <sakshi.chourasiya73899@gmail.com>
2026-04-25 05:37:39 +0530

    SSL

--- src/Database/db.js
+++ src/Database/db.js
@@ -8,7 +8,10 @@ const database = new Client({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+   ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false
 })
 try{
     await database.connect();
